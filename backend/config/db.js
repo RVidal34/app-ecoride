d80bc343a61da036118d3745ad777dbf78eb085b
RVidal34 <145911235+RVidal34@users.noreply.github.com>
2024-12-24 15:23:44 +0100

    Initialisation de la partie backend du projet avec serveur express fonctionnel et connection aux BDD SQL et NoSQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,9 @@ const pool = mariadb.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  connectionLimit: 10, // Augmente la limite si nécessaire
+  acquireTimeout: 20000, // Augmente le timeout (en ms)
 });
 
 module.exports = pool;
--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,7 @@ app.use(bodyParser.json());
 
 // Connexion MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connecté avec succès"))
   .catch((err) => console.error("Erreur de connexion à MongoDB:", err));
 
