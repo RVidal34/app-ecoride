49c25075952b6b6c88656105b9cede1139d35395
RVidal34 <145911235+RVidal34@users.noreply.github.com>
2025-01-03 13:49:02 +0100

    Installation de SASS pour les styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,8 @@
+// Importation du router
 import AppRouter from "./router/AppRouter";
+
+// Importation du fichier .css requis pour l'affichage des styles
+import "./styles/main.css";
 
 function App() {
   return (
